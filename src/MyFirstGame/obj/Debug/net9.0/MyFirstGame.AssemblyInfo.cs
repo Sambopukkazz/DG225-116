16cdc38504b7570c14282878d68567330c97267f
techin jarounsing <sambopukka062@gmail.com>
2026-06-23 21:27:56 +0700

    Changes2

--- src/MyFirstGame/obj/Debug/net9.0/MyFirstGame.AssemblyInfo.cs
+++ src/MyFirstGame/obj/Debug/net9.0/MyFirstGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e57ffd78bccf5eff28391ca773b327233f621b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
